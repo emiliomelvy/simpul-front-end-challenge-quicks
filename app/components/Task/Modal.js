d52f8e1c760ad0637f69040e53eefa8b44762a77
emiliomelvy <melvyemilio@yahoo.co.id>
2023-12-12 22:56:09 +0700

    feat: modal for add task list

---
+++ app/components/Task/Modal.js
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState } from "react";
+
+const Modal = ({ showModal, setShowModal, task, setTask }) => {
+  const [tempTask, setTempTask] = useState({
+    task: "",
+    date: "",
+    description: "",
+  });
+
+  const handleTask = () => {
+    const newTask = {
+      id: task.length + 1,
+      task: tempTask.task,
+      date: tempTask.date,
+      description: tempTask.description,
+    };
+
+    setTask([...task, newTask]);
+    setShowModal(false);
+  };
+
+  return (
+    <div className="fixed z-40 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full">
+      <div className="w-full h-full bg-gray-500 opacity-40"></div>
+      <div className="bg-white w-[550px] h-[320px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute rounded-xl py-6 px-28">
+        <div className="flex flex-col">
+          <label htmlFor="task">Task Title :</label>
+          <input
+            name="task"
+            type="text"
+            className="border rounded mt-2 mb-5"
+            value={tempTask.task}
+            onChange={(e) => setTempTask({ ...tempTask, task: e.target.value })}
+          />
+          <label htmlFor="date">Deadline :</label>
+          <input
+            name="date"
+            type="date"
+            className="border rounded mt-2 mb-5"
+            value={tempTask.date}
+            onChange={(e) => setTempTask({ ...tempTask, date: e.target.value })}
+          />
+          <label htmlFor="task">Description :</label>
+          <input
+            name="task"
+            type="text"
+            className="border rounded mt-2 mb-5"
+            value={tempTask.description}
+            onChange={(e) =>
+              setTempTask({ ...tempTask, description: e.target.value })
+            }
+          />
+        </div>
+        <div className="flex justify-between mt-1.5">
+          <button
+            onClick={() => setShowModal(false)}
+            className="bg-red-500 hover:bg-red-400 text-white py-2 px-4 rounded"
+          >
+            Cancel
+          </button>
+          <button
+            className="bg-blue-1 hover:bg-[#478ced] text-white py-2 px-4 rounded"
+            onClick={handleTask}
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- app/components/Task/TaskList.js
+++ app/components/Task/TaskList.js
@@ -1,53 +1,86 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
+import Modal from "./Modal";
 import arrow from "../../../public/arrow.svg";
 import option from "../../../public/option-black.svg";
 import time from "../../../public/time.svg";
 import pen from "../../../public/pen.svg";
 
 const TaskList = () => {
+  const [task, setTask] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <section className="py-10">
-      <div className="flex justify-between">
-        <div className="flex gap-3">
-          <input type="checkbox" className="place-self-start mt-2" />
-          <div className="text-gray-2 flex flex-col gap-4">
-            <div className="font-medium">Set up appointment with Dr Blake</div>
-          </div>
-        </div>
-        <div className="flex gap-3 items-center">
-          <div className="text-sm text-[#EB5757]">10 Days Left</div>
-          <div>22/06/2021</div>
-          <Image src={arrow} alt="expand" className="cursor-pointer" />
-          <Image
-            src={option}
-            alt="option"
-            width={16}
-            className="cursor-pointer"
-          />
-        </div>
+    <>
+      <div className="flex justify-between pb-5">
+        <select className="ml-20 border border-black rounded p-2">
+          <option value="mytask">My Task</option>
+        </select>
+        <button
+          onClick={() => setShowModal(true)}
+          className="bg-blue-1 rounded py-2 px-4 text-white"
+        >
+          New Task
+        </button>
       </div>
-      <div className="pl-7 pt-4 text-gray-2 pb-10">
-        <div className="flex gap-2.5 pb-5">
-          <Image src={time} alt="time" width={20} />
-          <input type="date" className="border p-2 rounded" />
-        </div>
-        <div className="flex gap-2.5">
-          <Image
-            src={pen}
-            alt="time"
-            width={20}
-            className="mb-auto cursor-pointer"
-          />
-          <div className="pr-12">
-            All Cases must include all payment transactions, all documents and
-            forms filled. All conversations in comments and messages in channels
-            and emails should be provided as well in.
+      {task.map((item) => {
+        return (
+          <div className={`pt-5 border-b`} key={item.id}>
+            <div className="flex justify-between">
+              <div className="flex gap-3">
+                <input type="checkbox" className="place-self-start mt-2" />
+                <div className="text-gray-2 flex flex-col gap-4">
+                  <div className="font-medium">{item.task}</div>
+                </div>
+              </div>
+              <div className="flex gap-3 items-center">
+                <div className="text-sm text-[#EB5757]">10 Days Left</div>
+                <div>{item.date}</div>
+                <Image src={arrow} alt="expand" className="cursor-pointer" />
+                <Image
+                  src={option}
+                  alt="option"
+                  width={16}
+                  className="cursor-pointer"
+                />
+              </div>
+            </div>
+            <div className="pl-7 pt-4 text-gray-2 pb-10">
+              <div className="flex gap-2.5 pb-5">
+                <Image src={time} alt="time" width={20} />
+                <input
+                  type="date"
+                  className="border p-2 rounded"
+                  value={item.date}
+                />
+              </div>
+              <div className="flex gap-2.5">
+                <Image
+                  src={pen}
+                  alt="time"
+                  width={20}
+                  className="mb-auto cursor-pointer"
+                />
+                <div className="pr-12 break-words w-[620px]">
+                  {item.description}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
+        );
+      })}
+
+      {showModal && (
+        <Modal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          task={task}
+          setTask={setTask}
+        />
+      )}
+    </>
   );
 };
 
